0ff95ca42342ded58eb3c81e8a4cd9504a81a992
Aubert Nungisa <aubert.nungisa@curling.ca>
2026-02-03 12:17:34 -0500

    feat: complete Sentry wizard setup

--- next.config.js
+++ next.config.js
@@ -189,9 +189,26 @@ const nextConfig = {
 module.exports = withSentryConfig(
   nextConfig,
   {
-    silent: true,
+    org: 'nzila-ventures',
+    project: 'abr_app',
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
   },
   {
     hideSourceMaps: true,
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: '/monitoring',
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    automaticVercelMonitors: true,
+    // Tree-shaking options for reducing bundle size
+    treeshake: {
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      removeDebugLogging: true,
+    },
   }
 )
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,9 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://4fb1319c553b5b586efa665813046965@o4509395283542016.ingest.de.sentry.io/4510823107067984",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
   enableLogs: true,
-  integrations: [
-    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
-  ],
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
